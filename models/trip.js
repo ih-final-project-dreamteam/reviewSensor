70bc8dea9626ad2a6628724e4b704d7df9905488
Eleyni Rodriguez <eleyni.m.rodriguez@gmail.com>
2018-05-28 17:22:43 -0400

    Add: Add dashboard styling and sorting by date.

--- models/trip.js
+++ models/trip.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
 const tripSchema = new Schema({
+  userId: {type: Schema.Types.ObjectId, ref: 'User'},
   tripName: { type: String, required: true },
   startDate: Date,
   endDate: Date,
   tripNotes: String,
-  userId: {type: Schema.Types.ObjectId, ref: 'User'},
-  userFavorites: [{type: Schema.Types.ObjectId, ref: 'Hotel'}],//this need to be modified to take yelp information
-//   flightInfo
+  hotel: Object
 },  
 {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
