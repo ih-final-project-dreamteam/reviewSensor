61bde23eb967edb805f273dc915bf9c5096c428c
Dali Rojas <rojasdali@gmail.com>
2018-06-01 02:22:38 -0400

    hia23

--- routes/watson-routes.js
+++ routes/watson-routes.js
@@ -71,8 +71,8 @@ watsonRoutes.post('/:searchTerm/:price/:id', (req, res, next) => {
 
             });
 
-        setTimeout(function () {myHotel.watson_sentiment = reviewAnalysis(myHotel.watson_sentiment)},1000)
-        setTimeout(function () {res.json(myHotel);},2000)
+        setTimeout(function () {myHotel.watson_sentiment = reviewAnalysis(myHotel.watson_sentiment)},1600)
+        setTimeout(function () {res.json(myHotel);},2400)
         
        
        
