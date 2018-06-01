0930cd4ffef132a5924416542f4a921610af854e
Dali Rojas <rojasdali@gmail.com>
2018-05-31 21:36:31 -0400

    y

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.use(passport.session());
 app.use(
   cors({
     credentials: true,                 // allow other domains to send cookies
-    origin: ["http://localhost:4200", "https://reviewsensor.herokuapp.com"]  // these are the domains that are allowed
+    origin: ["http://localhost:4200"]  // these are the domains that are allowed
   })
 );
 
